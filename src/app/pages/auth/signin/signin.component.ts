98c9e2e268b910fb8b4bbd0cb07f21f230290e4c
Perry Kwarteng <perry.kwarteng@amalitech.com>
2023-12-16 21:34:06 +0000

    added logic to log the signin details on the console

--- src/app/pages/auth/signin/signin.component.ts
+++ src/app/pages/auth/signin/signin.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.css'
+  styleUrl: './signin.component.css',
 })
 export class SigninComponent {
+  signinForm!: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.signinForm = this._fb.group({
+      email: [, [Validators.email, Validators.required]],
+      password: [, [Validators.required, Validators.minLength(5)]],
+      keepme: [, []],
+    });
+  }
 
+  signinfunction() {
+    if (this.signinForm.valid) {
+      console.log(this.signinForm.value);
+      this.signinForm.reset();
+    } else {
+      console.error('sorry');
+    }
+  }
 }
