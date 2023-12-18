8d8b29fe750cf7e33ea078cb8ed832bf8ff2ed61
Perry Kwarteng <perry.kwarteng@amalitech.com>
2023-12-18 13:52:51 +0000

    created the ui for signup page with logic for post request

--- src/app/pages/auth/signin/signin.component.ts
+++ src/app/pages/auth/signin/signin.component.ts
@@ -5,11 +5,12 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
 })
--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  signUpForm!: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.signUpForm = this._fb.group({
+      fullName: [, [Validators.required]],
+      email: [, [Validators.email, Validators.required]],
+      password: [, [Validators.required, Validators.minLength(5)]],
+    });
+  }
 
+  signinfunction() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+      this.signUpForm.reset();
+    } else {
+      console.error('sorry');
+    }
+  }
 }
