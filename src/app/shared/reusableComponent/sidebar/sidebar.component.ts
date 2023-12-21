3cca1dfca5ba664f0f179fa70009611336036324
Perry Kwarteng <perry.kwarteng@amalitech.com>
2023-12-21 14:40:06 +0000

    login on asidebar

--- src/app/shared/reusableComponent/sidebar/sidebar.component.ts
+++ src/app/shared/reusableComponent/sidebar/sidebar.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  isOpen: boolean = true;
+  isClose: boolean = false;
 
+  toggel() {
+    this.isOpen = !this.isOpen;
+    this.isClose = !this.isClose;
+  }
 }
