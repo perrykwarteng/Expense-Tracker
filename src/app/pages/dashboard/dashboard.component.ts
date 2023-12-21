085de363333b2fbfb558a290b65d76be39d1678d
Perry Kwarteng <perry.kwarteng@amalitech.com>
2023-12-21 13:24:40 +0000

    working on the aside bar as a sperate component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/reusableComponent/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
